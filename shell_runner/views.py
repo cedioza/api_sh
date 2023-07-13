1da3392c9793342d6e7cf2c5a59458565cbc29b2
cedioza <cedioza@misena.edu.co>
2023-07-13 14:36:20 -0500

    test route

--- shell_runner/views.py
+++ shell_runner/views.py
@@ -8,24 +8,61 @@ from .Helpers import helper
 logger = logging.getLogger(__name__)
 
 
+# @api_view(['GET'])
+# def execute_shell_command(request):
+#     try:
+#         # logger.info('Start process...')
+#         # logger.info('create folder files')
+
+
+#         # helper.create_folder('files')
+#         # logger.info('create folder logs')
+#         # helper.create_folder('logs')
+
+#         # helper.execute_process()
+#         # DIR_PATH = os.getcwd() + os.environ.get('DIR_PROJECT', '')
+#         # logger.info('DIR_PATH: ' + DIR_PATH)
+
+
+
+#         logger.info('End process...')
+
+#         return Response({'message': 'prueba', 'dir': DIR_PATH, 'data_ext': os.getenv('DIR_PROJECT')}, status=200)
+
+#     except Exception as e:
+#         error_message = 'An error occurred: {}'.format(str(e))
+#         logger.error(error_message)
+#         return Response({'error': error_message,"directorio":os.environ.get('DIR_PROJECT', '--')}, status=500)
+
 @api_view(['GET'])
 def execute_shell_command(request):
     try:
+
+
         logger.info('Start process...')
-        logger.info('create folder files')
-        helper.create_folder('files')
-        logger.info('create folder logs')
-        helper.create_folder('logs')
+        # Obtener la ruta del directorio principal (home)
+        directorio_principal = os.path.expanduser("~")
+        logger.info('Start Home ', directorio_principal)
 
-        helper.execute_process()
-        DIR_PATH = os.getcwd() + os.environ.get('DIR_PROJECT', '')
-        logger.info('DIR_PATH: ' + DIR_PATH)
+        # Navegar dos niveles más en las rutas específicas
+        ruta1 = os.path.join(directorio_principal, "colmena")
+        logger.info('Start colmena ', directorio_principal)
 
-        logger.info('End process...')
+        directorio_actual = os.path.join(ruta1, "colmena_cobros")
+        logger.info('Start colmena_cobros ', directorio_principal)
 
-        return Response({'message': 'prueba', 'dir': DIR_PATH, 'data_ext': os.getenv('DIR_PROJECT')}, status=200)
+
+
+        # Obtener la lista de archivos en la ruta final
+        archivos_en_ruta_objetivo = os.listdir(directorio_actual)
+        logger.info('Start colmena_cobros ', archivos_en_ruta_objetivo)
+
 
     except Exception as e:
         error_message = 'An error occurred: {}'.format(str(e))
         logger.error(error_message)
         return Response({'error': error_message,"directorio":os.environ.get('DIR_PROJECT', '--')}, status=500)
+
+
+    # Devolver la lista de archivos en la respuesta
+    return Response(archivos_en_ruta_objetivo)
