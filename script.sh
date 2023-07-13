bc95c6e751aaa3b1c34a582979bc6664670a8215
cedioza <cedioza@misena.edu.co>
2023-07-13 17:28:24 -0500

    test shell

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Realizar un ls antes de ejecutar el archivo Python
+ls
+
+# Ejecutar el archivo Python con Python 3.10
+python3.10 api_test.py
+
+# Realizar otro ls después de ejecutar el archivo Python
+ls
--- shell_runner/views.py
+++ shell_runner/views.py
@@ -51,3 +51,47 @@ def execute_shell_command(request):
         error_message = 'Ocurrió un error: {}'.format(str(e))
         logger.exception(error_message)
         return Response({'error': error_message}, status=500)
+    
+import os
+import subprocess
+import logging
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+logger = logging.getLogger(__name__)
+
+@api_view(['GET'])
+def execute_shell_command(request):
+    try:
+        # Obtener la ruta actual
+        ruta_actual = os.getcwd()
+
+        logger.info(f"Ruta por variable de entorno: {os.environ.get('DIR_PROJECT', 'no_fount')}")
+        logger.info(f'Ruta actual: {ruta_actual}')
+
+        # Construir la ruta completa
+        ruta_deseada = os.path.join(ruta_actual, "var", "www", "html", "api_sh")
+
+        # Cambiar al directorio deseado
+        os.chdir(ruta_deseada)
+
+        # Obtener una lista de elementos en la ruta deseada
+        elementos = os.listdir(ruta_deseada)
+
+        logger.info(f'Elementos en la ruta "{ruta_deseada}": {elementos}')
+
+        # Leer y ejecutar el archivo shell en la ruta deseada
+        archivo_sh = os.path.join(ruta_deseada, "script.sh")
+        resultado = subprocess.run(['sh', archivo_sh], capture_output=True, text=True)
+
+        logger.info(f'Archivo shell ejecutado correctamente. Salida: {resultado.stdout}')
+
+        # Restablecer la ruta actual
+        os.chdir(ruta_actual)
+
+        return Response({'ruta_actual': ruta_actual, 'elementos_ruta': elementos})
+
+    except Exception as e:
+        error_message = 'Ocurrió un error: {}'.format(str(e))
+        logger.exception(error_message)
+        return Response({'error': error_message}, status=500)
